a30aa107b7f9395ddf74b498a45e1dfad63b0cbf
Pradyumna Gupta <pradyg29@gmail.com>
2020-09-20 22:24:18 +0530

    Intro to Redux

---
+++ src/redux/storeConfiguration.js
@@ -0,0 +1,11 @@
+import { Form } from 'reactstrap';
+import {createStore} from 'redux';
+import {Reducer, initialState} from './reducer';
+
+export const ConfigureStore =() => {
+    const store = createStore(
+        Reducer,
+        initialState 
+    );
+    return store;
+}
