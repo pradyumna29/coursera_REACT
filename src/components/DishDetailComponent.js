3eb3aa1db1c46e98ca9e54937fe80a2e4bc2cb23
Pradyumna Gupta <pradyg29@gmail.com>
2020-09-22 20:16:15 +0530

    Redux actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -22,8 +22,8 @@ class CommentForm extends Component{
         }); 
     }
     handleSubmit(values){
-        console.log("current state is:" + JSON.stringify(values));
-        alert("current state is:" + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
     }
     render(){
 
@@ -48,12 +48,12 @@ class CommentForm extends Component{
                             </Row>
                             <Row className="form-group">
                                 <Col>
-                                    <Label htmlFor="yourname">Your Name</Label>
-                                    <Control.text model=".yourname" id="yourname" className="form-control" name="yourname" placeholder="Your Name"
+                                    <Label htmlFor="author">Your Name</Label>
+                                    <Control.text model=".author" id="author" className="form-control" name="author" placeholder="Your Name"
                                     validators={{required, maxLength: maxLength(15), minLength: minLength(3)}} />
                                     <Errors
                                         className="text-danger"
-                                        model=".yourname"
+                                        model=".author"
                                         show="touched"
                                         messages={{
                                             required: "Required ",
@@ -99,7 +99,7 @@ function RenderDish({ dish }) {
 }
 
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments != null) {
         const coms = comments.map((comment) => {
             return (
@@ -122,7 +122,7 @@ function RenderComments({ comments }) {
                 <ul className="list-unstyled">
                     {coms}
                 </ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment= {addComment} />
 
             </div>
         );
@@ -150,7 +150,9 @@ function RenderComments({ comments }) {
                             </div>
                             <div className="row">
                                 <RenderDish dish={props.dish} />
-                                <RenderComments comments={props.comments} />
+                                <RenderComments comments={props.comments}
+                                addComment={props.addComment}
+                                dishId={props.dish.id} />
                             </div>
                         </div>
                     );
